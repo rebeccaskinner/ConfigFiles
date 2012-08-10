2b86ab725f0d6f8553540c093ebbb2b949f9fb65
Rebecca Skinner <rebecca@rebeccaskinner.net>
2012-08-10 10:17:35 -0500

    added screen rotation to xmonad config

--- personal/xmonad.hs
+++ personal/xmonad.hs
@@ -1,22 +1,84 @@
 import XMonad
+import XMonad.Operations
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.EZConfig
+import XMonad.Layout.Named
+import XMonad.Layout.Reflect
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.WindowNavigation
+import XMonad.Util.CustomKeys
+import XMonad.StackSet as W
+import Data.List (find)
 import System.IO
+import Control.Monad
+
+
+-- define layouts
+rzTall     = ResizableTall 1 (3/100) (1/2) []
+fullLayout = named "Full" Full
+tallLayout = named "Tall" rzTall
+mirrorTall = named "Mirror" $ reflectHoriz rzTall
+wideLayout = named "Wide" $ Mirror rzTall
+
+myLayout = fullLayout ||| tallLayout ||| mirrorTall ||| wideLayout
+
+curLayout :: X String
+curLayout = gets windowset >>= return . description . W.layout . W.workspace . W.current
+
+vimDir k
+    | k == xK_h = L
+    | k == xK_j = D
+    | k == xK_k = U
+    | k == xK_l = R
+
+vimResizeMap k
+    | k == xK_h = sendMessage $ Shrink
+    | k == xK_j = sendMessage $ MirrorShrink
+    | k == xK_k = sendMessage $ MirrorExpand
+    | k == xK_l = sendMessage $ Expand
+
+vimResizeMapMirror k
+    | k == xK_h = sendMessage $ Expand
+    | k == xK_j = sendMessage $ MirrorShrink
+    | k == xK_k = sendMessage $ MirrorExpand
+    | k == xK_l = sendMessage $ Shrink
+
+vimGetResizeMap k = do
+    str <- curLayout
+    case str of
+        "Mirror"  -> vimResizeMapMirror k
+        otherwise -> vimResizeMap k
+    return ()
+
+vimKeys = [xK_h, xK_j, xK_k, xK_l]
+
+deleteList _ = [(i,j)|i<-[mod4Mask,customShiftMask,customCtrlMask],j<-vimKeys]
+
+keysList l = 
+       [((customShiftMask,x),sendMessage $ Swap (vimDir x))|x<-vimKeys]
+    ++ [((customCtrlMask ,x),vimGetResizeMap x) | x <- vimKeys]
+    ++ [((mod4Mask       ,x),sendMessage $ Go (vimDir x)) | x <- vimKeys]
+    ++ [((mod4Mask       , xK_Up), spawn "mm_portrait"),
+        ((mod4Mask       , xK_Down), spawn "mm_landscape")]
+
+customShiftMask = mod4Mask .|. shiftMask
+customCtrlMask  = mod4Mask .|. controlMask
 
 main = do
         xmproc <- spawnPipe "xmobar"
         xmonad $ defaultConfig 
             { manageHook = manageDocks <+> manageHook defaultConfig
-            , layoutHook = avoidStruts $ layoutHook defaultConfig
+            , layoutHook = windowNavigation $ avoidStruts myLayout
             , logHook = dynamicLogWithPP xmobarPP
                 { ppOutput = hPutStrLn xmproc
                 , ppTitle = xmobarColor "green" "" . shorten 50
                 }
             , modMask = mod4Mask
-            , borderWidth = 1
-            , terminal = "urxvt"
+            , keys = customKeys deleteList keysList
+            , borderWidth = 2
+            , terminal = "konsole"
             , normalBorderColor = "#333333"
-            , focusedBorderColor = "#5555cc"
+            , focusedBorderColor = "#FFAA00"
             }
